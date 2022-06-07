f3cc1e15853b7c645e334dc7e2156fbd70ceb813
Alexey <alexmiki2002@gmail.com>
2022-06-07 18:59:03 +0300

    Упражнение "На дне бассейна (Начало)"

---
+++ src/EchoTestDrive.java
@@ -0,0 +1,21 @@
+public class EchoTestDrive {
+    public static void main(String[] args) {
+        Echo e1 = new Echo();
+        //
+        int x = 0;
+        while (x >0) {
+            e1.hello();
+
+        }
+
+    }
+}
+ class Echo {
+    int x = 0;
+    void hello {
+        System.out.println("Привеееееет...");
+     }
+
+
+
+ }
